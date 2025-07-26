9cddbee6df92ab2cc1718f7757fe89f67f7aa284
prateek <prateekthakur272@gmail.com>
2025-07-26 23:15:44 +0530

    adds koin and view models

--- app/src/main/java/dev/prateekthakur/netlytix/ui/features/networkInfo/screens/NetworkDetailsScreen.kt
+++ app/src/main/java/dev/prateekthakur/netlytix/ui/features/networkInfo/screens/NetworkDetailsScreen.kt
@@ -1,15 +1,45 @@
 package dev.prateekthakur.netlytix.ui.features.networkInfo.screens
 
+import androidx.compose.animation.core.FastOutSlowInEasing
+import androidx.compose.animation.core.RepeatMode
+import androidx.compose.animation.core.animateFloat
+import androidx.compose.animation.core.infiniteRepeatable
+import androidx.compose.animation.core.rememberInfiniteTransition
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.PlayArrow
+import androidx.compose.material3.Card
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import dev.prateekthakur.netlytix.R
+import dev.prateekthakur.netlytix.domain.enums.NetworkConnectionType
+import dev.prateekthakur.netlytix.domain.models.NetworkConnectionInfo
+import dev.prateekthakur.netlytix.domain.models.WifiConnectionInfo
 import dev.prateekthakur.netlytix.ui.features.networkInfo.viewmodels.NetworkInfoViewModel
 import dev.prateekthakur.netlytix.ui.features.networkInfo.viewmodels.WifiConnectionInfoViewModel
 import org.koin.androidx.compose.koinViewModel
-
 
 @Composable
 fun NetworkDetailsScreen(
@@ -20,8 +50,137 @@ fun NetworkDetailsScreen(
     val network by networkInfoViewModel.state.collectAsState()
     val wifi by wifiConnectionInfoViewModel.state.collectAsState()
 
-    Column {
-        Text(network.toString())
-        Text(wifi.toString())
+    Scaffold() { safeArea ->
+        Column(
+            modifier = modifier
+                .padding(safeArea)
+                .fillMaxWidth()
+                .padding(16.dp),
+            verticalArrangement = Arrangement.spacedBy(16.dp)
+        ) {
+            BasicNetworkDetailsView(network)
+            NetworkTestButton()
+            WifiNetworkInfoView(wifi)
+        }
+    }
+}
+
+@Composable
+fun NetworkTestButton(modifier: Modifier = Modifier) {
+    Card {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceBetween,
+            modifier = modifier
+                .padding(16.dp)
+                .fillMaxWidth()
+        ) {
+            Text("Run Network Tests", style = MaterialTheme.typography.titleMedium)
+            IconButton(onClick = {}) { Icon(Icons.Default.PlayArrow, contentDescription = "Test") }
+        }
+    }
+}
+
+@Composable
+fun BasicNetworkDetailsView(info: NetworkConnectionInfo, modifier: Modifier = Modifier) {
+
+    val isConnected = info.type != NetworkConnectionType.NONE
+
+    Card {
+        Column(
+            modifier = modifier
+                .fillMaxWidth()
+                .padding(16.dp)
+        ) {
+
+            Text("Connection Status", style = MaterialTheme.typography.labelMedium)
+            Text(
+                if (isConnected) "Connected" else "Disconnected",
+                style = MaterialTheme.typography.titleMedium
+            )
+
+            val pulseAnimation = rememberInfiniteTransition()
+            val scale by pulseAnimation.animateFloat(
+                initialValue = 1f, targetValue = 1.1f, animationSpec = infiniteRepeatable(
+                    animation = tween(800, easing = FastOutSlowInEasing),
+                    repeatMode = RepeatMode.Reverse
+                )
+            )
+
+            Box(contentAlignment = Alignment.Center, modifier = modifier.fillMaxWidth()) {
+
+                Image(
+                    painter = painterResource(
+                        id = if (info.type == NetworkConnectionType.NONE) {
+                            R.drawable.not_connected
+                        } else {
+                            R.drawable.connected
+                        }
+                    ), contentDescription = null, modifier = Modifier
+                        .size(160.dp)
+                        .graphicsLayer(
+                            scaleX = scale, scaleY = scale
+                        )
+                )
+            }
+
+            Text(info.type.name, style = MaterialTheme.typography.titleLarge)
+            HorizontalDivider(modifier = modifier.padding(vertical = 8.dp))
+
+            Row {
+                Text(
+                    "Internet : ${if (!info.internetAccessible) "Disconnected" else "Connected"}",
+                    style = MaterialTheme.typography.labelLarge
+                )
+            }
+            Text(
+                "VPN : ${if (!info.vpnConnected) "Not connected" else "Connected"}",
+                style = MaterialTheme.typography.labelLarge
+            )
+            Text(
+                "Gateway: ${info.dnsServer.joinToString { "$it " }}",
+                style = MaterialTheme.typography.labelLarge
+            )
+        }
+    }
+}
+
+@Composable
+fun WifiNetworkInfoView(info: WifiConnectionInfo, modifier: Modifier = Modifier) {
+    Card {
+        Column(
+            modifier = modifier
+                .padding(16.dp)
+                .fillMaxWidth()
+        ) {
+
+            Text("Wifi Details", style = MaterialTheme.typography.titleMedium)
+
+            Box(modifier = modifier.fillMaxWidth(), contentAlignment = Alignment.Center) {
+                Image(
+                    painter = painterResource(R.drawable.wifi),
+                    contentDescription = "",
+                    modifier = modifier.size(120.dp)
+                )
+            }
+
+            Spacer(modifier = modifier.height(12.dp))
+
+            Text("SSID : ${info.ssid}", style = MaterialTheme.typography.labelLarge)
+            Text("BSSID : ${info.bssid}", style = MaterialTheme.typography.labelLarge)
+            Text(
+                "Strength : ${info.signalStrength} dbm",
+                style = MaterialTheme.typography.labelLarge
+            )
+            Text("Hidden : ${info.isSSidHidden}", style = MaterialTheme.typography.labelLarge)
+            Text("Network id : ${info.networkId}", style = MaterialTheme.typography.labelLarge)
+            Text("Frequency : ${info.frequency} hz", style = MaterialTheme.typography.labelLarge)
+            Text("Link speed : ${info.linkSpeed} mbps", style = MaterialTheme.typography.labelLarge)
+            Text("Gateway : ${info.gatewayAddress}", style = MaterialTheme.typography.labelLarge)
+            Text(
+                "Supplicant state : ${info.supplicantState}",
+                style = MaterialTheme.typography.labelLarge
+            )
+        }
     }
 }
